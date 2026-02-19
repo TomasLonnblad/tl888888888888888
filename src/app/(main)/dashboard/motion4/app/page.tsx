10f11876c3c0185ff2acddce8f4bd9b2288ac134
TomasLonnblad <tomaslonnblad@gmail.com>
2026-02-19 09:31:46 +0100

    x

---
+++ src/app/(main)/dashboard/motion4/app/page.tsx
@@ -0,0 +1,157 @@
+
+
+import {
+  Field,
+  FieldContent,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldTitle,
+} from "@/components/ui/field"
+import { Label } from "@/components/ui/label"
+import { Switch } from "@/components/ui/switch"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Separator } from "@/components/ui/separator"
+import { Badge } from "@/components/ui/badge"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import Image from "next/image"
+import { AspectRatio } from "@/components/ui/aspect-ratio"
+
+
+
+
+
+export default function Page() {
+  return (
+    <div className="flex items-center space-x-2">
+
+
+
+          <a href="https://tomaslonnblad.github.io/tleeee">
+        
+       
+      <Switch id="airplane-mode" /> </a>
+      <Label htmlFor="airplane-mode">3D Mode</Label>
+        <Label htmlFor="terms">Accept terms and conditions</Label>
+             <Checkbox id="terms" />
+          
+
+
+
+    <FieldGroup className="max-w-sm">
+      <Field orientation="horizontal">
+        <Checkbox id="terms-checkbox" name="terms-checkbox" />
+        <Label htmlFor="terms-checkbox">Accept terms and conditions</Label>
+      </Field>
+      <Field orientation="horizontal">
+        <Checkbox
+          id="terms-checkbox-2"
+          name="terms-checkbox-2"
+          defaultChecked
+        />
+        <FieldContent>
+          <FieldLabel htmlFor="terms-checkbox-2">
+            Accept terms and conditions
+          </FieldLabel>
+          <FieldDescription>
+            By clicking this checkbox, you agree to the terms.
+          </FieldDescription>
+        </FieldContent>
+      </Field>
+      <Field orientation="horizontal" data-disabled>
+        <Checkbox id="toggle-checkbox" name="toggle-checkbox" disabled />
+        <FieldLabel htmlFor="toggle-checkbox">Enable notifications</FieldLabel>
+      </Field>
+      <FieldLabel>
+        <Field orientation="horizontal">
+          <Checkbox id="toggle-checkbox-2" name="toggle-checkbox-2" />
+          <FieldContent>
+            <FieldTitle>Enable notifications</FieldTitle>
+            <FieldDescription>
+              You can enable or disable notifications at any time.
+            </FieldDescription>
+          </FieldContent>
+        </Field>
+      </FieldLabel>
+    </FieldGroup>
+
+
+
+
+
+      <Badge className="bg-blue-50 text-blue-700 dark:bg-blue-950 dark:text-blue-300">
+        Blue
+      </Badge>
+      <Badge className="bg-green-50 text-green-700 dark:bg-green-950 dark:text-green-300">
+        Green
+      </Badge>
+      <Badge className="bg-sky-50 text-sky-700 dark:bg-sky-950 dark:text-sky-300">
+        Sky
+      </Badge>
+      <Badge className="bg-purple-50 text-purple-700 dark:bg-purple-950 dark:text-purple-300">
+        Purple
+      </Badge>
+      <Badge className="bg-red-50 text-red-700 dark:bg-red-950 dark:text-red-300">
+        Red
+      </Badge>
+
+
+
+
+    <Card className="relative mx-auto w-full max-w-sm pt-0">
+      <div className="absolute inset-30 z-10 aspect-video bg-black/15" />
+      <img
+        src="https://tomaslonnblad.github.io/2.jpg"
+        alt="Event cover"
+        className="relative z-20 aspect-video w-full object-cover brightness-60  dark:brightness-40"
+      />
+
+
+
+      <CardHeader>
+        <CardAction>
+          <Badge variant="secondary">Featured</Badge>
+        </CardAction>
+        <CardTitle>Design systems meetup</CardTitle>
+        <CardDescription>
+          A practical talk on component APIs, accessibility, and shipping
+          faster.
+        </CardDescription>
+      </CardHeader>
+      <CardFooter>
+        <Button className="w-full">View Event</Button>
+      </CardFooter>
+    </Card>
+
+
+
+      <AspectRatio ratio={0/0 } className="bg-muted rounded-lg">
+        
+        <img
+          src="https://tomaslonnblad.github.io/2.jpg"
+          alt="Img"
+          fill={true}
+          className="rounded-lg object-cover grayscale dark:brightness-20"
+        />
+      </AspectRatio>
+
+
+    </div>
+  )
+}
+
+
+
+
+     
+     
